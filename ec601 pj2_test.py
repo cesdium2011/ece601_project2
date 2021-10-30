6e17c093fc1ea38f5a4b3a1353c216bf92933444
cesdium2011 <90536493+cesdium2011@users.noreply.github.com>
2021-10-29 23:28:28 -0400

    Add files via upload

---
+++ ec601 pj2_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 22:18:01 2021
+
+@author: cesdi
+"""
+
+def test_hashtagcheck(user_input):
+    if user_input is False:
+        new_user_input = input('Hashtag requested does not exist. Try another one:')
+        return new_user_input 
+    else:
+        return user_input
+'''
+def test_tweetscheck(user_input):
+    #pass out all tweets that the languages suppported by twiiter
+    for tweets in :
+        if tweets is True:
+            return tweets
+    
+def test_frequentrequest(user_input):
+    counter = 0
+    if counter
+    
+    return 
+'''
